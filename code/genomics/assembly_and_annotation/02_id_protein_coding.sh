48398d63e4292b03874e2e3f0ce3933828c03f44
KevinDRicks <149696275+KevinDRicks@users.noreply.github.com>
2026-05-28 16:46:42 -0400

    Add script for protein-coding gene annotation

---
+++ code/genomics/assembly_and_annotation/02_id_protein_coding.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#code passes the reference ancestor genome through prokka for annotation
+#primary utility here is for an easy passage through prodigal for identifying protein-coding genes 
+#note: this script was initially run on Compute Canada server
+
+#load libraries
+module load StdEnv/2020
+module load gcc/9.3.0
+module load prokka/1.14.5
+
+prokka /path/to/ancestor/allorhizobium_anc_assembly.fasta --outdir /prokka/output/predicted/proteincoding/genes
+#output will include prokka_output.gff & prokka_output.tsv & prokka_output.faa
+#these can be found in the data folder in on the git repo 
+#PROKKA_05052025.gff
+#PROKKA_05052025.tsv
+#PROKKA_05052025.faa
